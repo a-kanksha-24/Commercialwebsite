8f5b6b733de2d3cd4cca503b4cf616d2f3cf0dbe
a-kanksha-24 <98012700+a-kanksha-24@users.noreply.github.com>
2023-04-22 13:55:31 +0530

    Initial commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/1.jpg";
+import product2 from "./assets/2.jpg";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.jpeg";
+import product6 from "./assets/6.webp";
+import product7 from "./assets/7.jpg";
+import product8 from "./assets/8.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Nariyal's laddoo",
+    price: 200,
+    productImage: product1
+  },
+  {
+    id: 2,
+    productName: "Kaju katli",
+    price: 200,
+    productImage: product2
+  },
+  {
+    id: 3,
+    productName: "Bundi ka laddu",
+    price: 200,
+    productImage: product3
+  },
+  {
+    id: 4,
+    productName: "Gulab jamun",
+    price: 200,
+    productImage: product4
+  },
+  {
+    id: 5,
+    productName: "Doda Barfi",
+    price: 200,
+    productImage: product5
+  },
+  {
+    id: 6,
+    productName: "Rasgulla",
+    price: 200,
+    productImage: product6
+  },
+  {
+    id: 7,
+    productName: "Rasmalai",
+    price: 200,
+    productImage: product7
+  },
+  {
+    id: 8,
+    productName: "Jaunpur's Imarti",
+    price: 200,
+    productImage: product8
+  }
+];
